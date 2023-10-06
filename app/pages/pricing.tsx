b45caa6b811ea04966a917606567e8705d176ab4
Adam Rodriguez <adamxrodriguez@gmail.com>
2023-10-06 16:22:44 -0400

    aider: Add a new section on the pricing page with steps to sell a home to our company.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- app/pages/pricing.tsx
+++ app/pages/pricing.tsx
@@ -9,6 +9,18 @@ export default function PricingPage() {
       <Wrapper>
         <PricingTablesSection />
         <FaqSection />
+
+        <StepsSection>
+          <StepsTitle>Steps to Sell Your Home to Our Company</StepsTitle>
+          <StepsList>
+            <StepItem>Step 1: Contact us to schedule a home evaluation.</StepItem>
+            <StepItem>Step 2: Our team will visit your home and assess its value.</StepItem>
+            <StepItem>Step 3: We will provide you with a fair cash offer for your home.</StepItem>
+            <StepItem>Step 4: If you accept the offer, we will proceed with the paperwork.</StepItem>
+            <StepItem>Step 5: We will handle all the necessary legal and financial processes.</StepItem>
+            <StepItem>Step 6: Once everything is finalized, we will close the deal and purchase your home.</StepItem>
+          </StepsList>
+        </StepsSection>
       </Wrapper>
     </Page>
   );
@@ -18,4 +30,22 @@ const Wrapper = styled.div`
   & > :last-child {
     margin-bottom: 15rem;
   }
+`;
+
+const StepsSection = styled.section`
+  margin-top: 4rem;
+`;
+
+const StepsTitle = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 2rem;
+`;
+
+const StepsList = styled.ul`
+  list-style-type: decimal;
+  padding-left: 2rem;
+`;
+
+const StepItem = styled.li`
+  margin-bottom: 1rem;
 `;
