be820436089b61c3e703322319908ca4bfb11d8a
Adam Rodriguez <adamxrodriguez@gmail.com>
2023-10-06 16:22:42 -0400

    Refactor pricing page to use new components and styling.

--- app/pages/pricing.tsx
+++ app/pages/pricing.tsx
@@ -1,53 +1,21 @@
-import React from 'react';
 import styled from 'styled-components';
+import Page from 'components/Page';
+import FaqSection from 'views/PricingPage/FaqSection';
+import PricingTablesSection from 'views/PricingPage/PricingTablesSection';
 
-const PricingWrapper = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
-`;
-
-const PricingTitle = styled.h1`
-  text-align: center;
-  margin-bottom: 20px;
-`;
-
-const PricingItem = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 10px;
-`;
-
-const ItemName = styled.span`
-  font-weight: bold;
-`;
-
-const ItemPrice = styled.span`
-  color: #007bff;
-`;
-
-function Pricing() {
+export default function PricingPage() {
   return (
-    <PricingWrapper>
-      <PricingTitle>Pricing</PricingTitle>
-
-      <PricingItem>
-        <ItemName>Basic Plan</ItemName>
-        <ItemPrice>$9.99/month</ItemPrice>
-      </PricingItem>
-
-      <PricingItem>
-        <ItemName>Pro Plan</ItemName>
-        <ItemPrice>$19.99/month</ItemPrice>
-      </PricingItem>
-
-      <PricingItem>
-        <ItemName>Premium Plan</ItemName>
-        <ItemPrice>$29.99/month</ItemPrice>
-      </PricingItem>
-    </PricingWrapper>
+    <Page title="Pricing" description="Cupidatat et reprehenderit ullamco aute ullamco anim tempor.">
+      <Wrapper>
+        <PricingTablesSection />
+        <FaqSection />
+      </Wrapper>
+    </Page>
   );
 }
 
-export default Pricing;
+const Wrapper = styled.div`
+  & > :last-child {
+    margin-bottom: 15rem;
+  }
+`;
